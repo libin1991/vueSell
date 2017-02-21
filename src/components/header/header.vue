f4e5f0a1e49ee09a447136cb1fc92fa383fbde69
Raoul <baobaozhou199606@gmail.com>
2017-02-21 12:27:07 +0800

    header外部组件开发完成

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -25,9 +25,11 @@
       </div>
     </div>
     <div class="bulletin-wrapper">
-      <span class="bulletin-title"></span>
-      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%" alt="">
     </div>
   </div>
 </template>
@@ -48,8 +50,9 @@
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixin.styl"
   .header
+    position :relative
     color: #fff
-    background: #999
+    background :rgba(7,17,27,.3)
     .content-wrapper
       position: relative
       padding: 24px 12px 18px 24px
@@ -122,11 +125,39 @@
           margin-left: 2px
           font-size: 10px
     .bulletin-wrapper
+      position: relative
       height: 28px
       line-height: 28px
       padding: 0 22px 0 12px
       white-space: nowrap
       overflow: hidden
       text-overflow: ellipsis
+      background :rgba(7,17,27,.2)
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        margin-top: 7px
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        margin: 0 4px
+        font-size: 10px
+        vertical-align: top
+      .icon-keyboard_arrow_right
+        position: absolute
+        right: 12px
+        top: 10px
+        font-size: 10px
       sadf: f
+    .background
+      position :absolute
+      top:0
+      left :0
+      width :100%
+      height :100%
+      z-index :-1
+      filter :blur(10px)
 </style>

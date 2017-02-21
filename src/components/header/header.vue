b25e08dc7421baa940a1f9c54019e50e5d7cff6e
Raoul <baobaozhou199606@gmail.com>
2017-02-21 13:44:31 +0800

    使用@click,体现了vuejs的双向数据绑定的特性

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,20 +17,21 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">
           {{seller.supports.length}}个
         </span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click="showDetail">
       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%" alt="">
     </div>
+    <div v-show="detailShow" class="detail" @click="hideDetail"></div>
   </div>
 </template>
 
@@ -39,6 +40,19 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data () {
+      return {
+          detailShow: false
+      };
+    },
+    methods: {
+      showDetail () {
+          this.detailShow = true;
+      },
+      hideDetail () {
+          this.detailShow = false;
       }
     },
     created () {
@@ -50,9 +64,10 @@
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixin.styl"
   .header
-    position :relative
+    position: relative
     color: #fff
-    background :rgba(7,17,27,.3)
+    overflow: hidden
+    background: rgba(7, 17, 27, .3)
     .content-wrapper
       position: relative
       padding: 24px 12px 18px 24px
@@ -132,11 +147,11 @@
       white-space: nowrap
       overflow: hidden
       text-overflow: ellipsis
-      background :rgba(7,17,27,.2)
+      background: rgba(7, 17, 27, .2)
       .bulletin-title
         display: inline-block
         vertical-align: top
-        margin-top: 7px
+        margin-top: 8px
         width: 22px
         height: 12px
         bg-image('bulletin')
@@ -153,11 +168,21 @@
         font-size: 10px
       sadf: f
     .background
-      position :absolute
-      top:0
-      left :0
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
+    .detail
+      position :fixed
+      z-index :100
+      top: 0
+      left: 0
       width :100%
       height :100%
-      z-index :-1
-      filter :blur(10px)
+      overflow :auto
+      background :rgba(7,17,27,.8)
+      dfd:f
 </style>

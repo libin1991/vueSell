6c97abf0478e903f251e9f1d40cc5d04289fe391
Raoul1996 <baobaozhou199606@gmail.com>
2017-03-08 20:48:47 +0800

    在food组件中添加了cartcontrol组件,并添加了transition动画

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,14 +1,32 @@
 <template>
-  <div class="food" v-show="showFlag" transition="move">
+  <div class="food" v-show="showFlag" transition="move" v-el:food>
     <div class="food-content">
       <div class="image-header">
-        <img :src="food.image" alt="" width="100%"/>
+        <img :src="food.image" alt=""/>
+        <div class="back" @click="hide"><i class="icon-arrow_lift"></i></div>
       </div>
+      <div class="content">
+        <h1 class="title">{{food.name}}</h1>
+        <div class="detail">
+          <span class="sell-count">月售{{food.sellCount}}份</span><span class="rating">好评率{{food.rating}}%</span>
+        </div>
+        <div class="price">
+          <span class="now">￥{{food.price}}</span>
+          <span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
+        </div>
+      </div>
+      <div class="cartcontrol-wrapper">
+        <cartcontrol :food="food"></cartcontrol>
+      </div>
+      <div @click.stop.pervent="addFirst" class="buy" v-show="!food.count || food.count ===0" transition="fade">加入购物车</div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Vue from 'vue';
+  import BScroll from 'better-scroll';
+  import cartcontrol from 'components/cartcontrol/cartcontrol';
   export default {
     props: {
       food: {
@@ -24,7 +42,28 @@
     methods: {
       show() {
         this.showFlag = true;
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$els.food, {
+              click: true
+            });
+          }
+        });
+      },
+      hide() {
+        this.showFlag = false;
+      },
+      // 不传参数,默认的参数是event
+      addFirst(event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.$dispatch('cart.add', event.target);
+        Vue.set(this.food, 'count', 1);
       }
+    },
+    components: {
+      cartcontrol
     }
   };
 </script>
@@ -43,4 +82,78 @@
       transform: translate3d(0, 0, 0)
     &.move-enter, &.move-leave
       transform: translate3d(100%, 0, 0)
+    .image-header
+      position: relative
+      width: 100%
+      height: 0
+      padding-top: 100%
+      img
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+      .back
+        position: absolute;
+        top: 10px
+        left: 0
+        .icon-arrow_lift
+          display: block
+          padding: 10px
+          font-size: 20px
+          color: #ffffff
+    .content
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 8px
+        font-size: 14px
+        font-weight: 700
+        color: rgb(7, 17, 27)
+      .detail
+        margin-bottom: 18px
+        line-height: 10px
+        height: 10px
+        font-size: 0
+        .sell-count, .rating
+          font-size: 10px
+          color: rgb(147, 153, 159)
+        .sell-count
+          margin-right: 12px
+      .price
+        font-weight: 700
+        line-height: 24px
+        .now
+          margin-right: 8px
+          font-size: 14px
+          color: rgb(240, 20, 20)
+        .old
+          text-decoration: line-through
+          color: rgb(147, 153, 159)
+  // TODO: 样式稍微有一些问题,使用负margin的方式进行解决,
+    .cartcontrol-wrapper
+      position: absolute
+      right: 12px
+      margin-top: -52px
+    /*bottom: 12px*/
+    .buy
+      position: absolute
+      right: 18px
+      /*bottom: 18px*/
+      margin-top: -44px
+      z-index: 10
+      height: 24px
+      line-height: 24px
+      padding: 0 12px
+      box-sizing: border-box
+      font-size: 10px
+      border-radius: 12px
+      color: #ffffff
+      background-color: rgb(0, 160, 220)
+      &.fade-transition
+        transition: all 0.2s
+        opacity: 1
+      &.fade-enter, &.fade-leave
+        opacity: 0
+      test: test
 </style>
